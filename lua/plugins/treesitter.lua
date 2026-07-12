0ecce72bbeb64e49b13d210c587be87ded264d75
mcchukwu <miraclechukwu2011@gmail.com>
2026-07-12 03:03:30 +0100

    refactored treesitter plugin config to new spec

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,36 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  opts = {
-    highlight = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-    },
-  },
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+
+  config = function()
+    local ts = require("nvim-treesitter")
+
+    ts.install({
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "bash",
+      "json",
+      "yaml",
+      "toml",
+      "markdown",
+      "markdown_inline",
+      "html",
+      "css",
+      "javascript",
+      "typescript",
+      "tsx",
+      "svelte",
+      "go",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+        vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
 }
